41af824cf4e37fe6839a9f5ccbff2a4af0c8c17c
Harry Sheppard <15553729+99-lives@users.noreply.github.com>
2023-03-04 15:52:44 +1100

    Improved user Schema and Update

--- msgraph/user_resource.go
+++ msgraph/user_resource.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
@@ -68,6 +70,9 @@ func (r *userResource) Schema(_ context.Context, _ resource.SchemaRequest, resp 
 			},
 			"id": schema.StringAttribute{
 				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
 			},
 		},
 	}
@@ -238,7 +243,6 @@ func (r *userResource) Update(ctx context.Context, req resource.UpdateRequest, r
 	}
 
 	// Update resource state with Computed values
-	plan.Id = types.StringValue(state.Id.ValueString())
 
 	diags = resp.State.Set(ctx, plan)
     resp.Diagnostics.Append(diags...)
