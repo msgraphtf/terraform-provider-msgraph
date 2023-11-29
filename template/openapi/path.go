143b4711f4b1b17a1715fad2bc5469613744d4bc
Harry Sheppard <15553729+99-lives@users.noreply.github.com>
2023-11-29 20:38:47 +1100

    Updated for new kin-openapi version

--- template/openapi/path.go
+++ template/openapi/path.go
@@ -30,7 +30,7 @@ func GetPath(pathname string) OpenAPIPathObject {
 	for _, param := range path.Get.Parameters.GetByInAndName("query", "$select").Schema.Value.Items.Value.Enum {
 		pathObject.Get.SelectParameters = append(pathObject.Get.SelectParameters, param.(string))
 	}
-	pathObject.Get.Response = GetSchemaObjectByRef(path.Get.Responses.Get(200).Value.Content.Get("application/json").Schema.Ref)
+	pathObject.Get.Response = GetSchemaObjectByRef(path.Get.Responses.Status(200).Value.Content.Get("application/json").Schema.Ref)
 
 	return pathObject
 }
