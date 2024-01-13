c45c857107a599723fa7b2e501357d7857fe9f41
Harry Sheppard <15553729+99-lives@users.noreply.github.com>
2024-01-13 18:16:13 +1100

    different testing

--- testing/testing.go
+++ testing/testing.go
@@ -3,12 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"reflect"
 
 	azidentity "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
-	"github.com/microsoftgraph/msgraph-sdk-go/models/odataerrors"
-	"github.com/microsoftgraph/msgraph-sdk-go/users"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
+	"github.com/microsoft/kiota-abstractions-go/serialization"
 )
 
 func main() {
@@ -16,50 +15,27 @@ func main() {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 
 	if err != nil {
-		fmt.Printf("Error creating credentials: %v\n", err)
+		fmt.Printf("Error creating credentials: %s\n", err)
 	}
 
 	client, err := msgraphsdk.NewGraphServiceClientWithCredentials(cred, []string{"https://graph.microsoft.com/.default"})
 	if err != nil {
-		fmt.Printf("Error creating client: %v\n", err)
+		fmt.Printf("Error creating client: %s\n", err)
 		return
 	}
 
-	qparams := users.UserItemRequestBuilderGetRequestConfiguration{
-		QueryParameters: &users.UserItemRequestBuilderGetQueryParameters{
-			Select: []string{"PasswordProfile"},
-		},
-	}
-
-	result, err := client.Users().ByUserId("0acc8010-50ea-4a54-bd71-ec485d425a74").Get(context.Background(), &qparams)
+	requestBody := models.NewUser()
+	myId := "82d80745-e0a8-4254-9b93-964f8f0a7672"
+	myName := ""
+	requestBody.SetGivenName(&myName)
+	_, err = client.Users().ByUserId(myId).Patch(context.Background(), requestBody, nil)
 	if err != nil {
-		fmt.Printf("Error getting the user: %v\n", err)
-		printOdataError(err)
+		fmt.Printf("Error patching user\n")
+		fmt.Printf("%s\n", err.Error())
 	}
-	fmt.Printf("Found User : %+v\n", *result.GetPasswordProfile().GetForceChangePasswordNextSignIn())
 
-	client.Users().ByUserId("").Activities().Get()
+	userObject, err := serialization.SerializeToJson(requestBody)
+	fmt.Printf("%+v\n", string(userObject))
+	fmt.Printf("%+v\n", err)
 
-	fmt.Printf("%+v\n", reflect.ValueOf(client.Users().ByUserId("0acc8010-50ea-4a54-bd71-ec485d425a74")).MethodByName("Get").Type())
-
-	value, _ := reflect.TypeOf(client.Users()).MethodByName("ByUserId")
-	value, _ = value.Type.Out(0).MethodByName("Get")
-	method := value.Type.In(2).Elem()
-	value2, _ := method.FieldByName("QueryParameters")
-	fmt.Printf("%+v\n", value2.Type)
-
-}
-
-func printOdataError(err error) {
-	switch err.(type) {
-	case *odataerrors.ODataError:
-		typed := err.(*odataerrors.ODataError)
-		fmt.Printf("error:", typed.Error())
-		if terr := typed.GetErrorEscaped(); terr != nil {
-			fmt.Printf("code: %s", *terr.GetCode())
-			fmt.Printf("msg: %s", *terr.GetMessage())
-		}
-	default:
-		fmt.Printf("%T > error: %#v", err, err)
-	}
 }
