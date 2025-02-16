44b4096115928129c1bfe4eb548782e187b9af8d
Harry Sheppard <15553729+99-lives@users.noreply.github.com>
2025-02-16 16:48:56 +1100

    Added doc.go to openapi

---
+++ generate/openapi/doc.go
@@ -0,0 +1,9 @@
+package openapi
+
+/*
+
+The openapi package contains functions required to parse the data structure that kin-openapi provides when loading the msgraph OpenAPI definition.
+
+It extracts the OAPI schema properties from given paths, using recursion to move up and down the schema hierarchy when needed.
+
+*/
