848de49819680641212391d78256376b951e045c
Harry Sheppard <15553729+99-lives@users.noreply.github.com>
2025-05-13 21:06:59 +1000

    Simplified openapi doc loading

--- generate/openapi/openapi.go
+++ generate/openapi/openapi.go
@@ -3,18 +3,14 @@ package openapi
 import (
 	"fmt"
 	"github.com/getkin/kin-openapi/openapi3"
-	"path/filepath"
-	"runtime"
 )
 
 var doc *openapi3.T = loadOpenAPISchema()
 
 func loadOpenAPISchema() *openapi3.T {
-	_, b, _, _ := runtime.Caller(0)
-	root := filepath.Join(filepath.Dir(b), "../..")
 
 	fmt.Println("Loading")
-	doc, err := openapi3.NewLoader().LoadFromFile(root + "/msgraph-metadata/openapi/v1.0/openapi.yaml")
+	doc, err := openapi3.NewLoader().LoadFromFile("./msgraph-metadata/openapi/v1.0/openapi.yaml")
 	if err != nil {
 		panic(err)
 	}
